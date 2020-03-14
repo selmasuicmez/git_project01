794e34fe6c3589abdafb768002fc823365c2ac99
Selma Suicmez <selmasuicmez@gmail.com>
2020-03-14 21:07:34 +0100

    commit 6 added

--- src/gitPackage/GitDay2.java
+++ src/gitPackage/GitDay2.java
@@ -6,5 +6,6 @@ public class GitDay2 {
 	public static void main(String[] Args) {
 		System.out.println("Hello world");
 		System.out.println("Hello Git Users!");
+		System.out.println("check if your local is ahead with your origin master");
 	}
 }
